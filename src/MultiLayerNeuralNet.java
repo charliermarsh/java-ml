0a07ab1917905e24f4386436a013e052be351299
Charles Marsh <crmarsh416@gmail.com>
2013-01-06 09:24:45 -0500

    fixed MLNN, which is now working properly

--- src/MultiLayerNeuralNet.java
+++ src/MultiLayerNeuralNet.java
@@ -2,16 +2,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.LinkedList;
 
-
 public class MultiLayerNeuralNet implements Classifier {
 	/* algorithm's learning rate. */
-	private final double learning_rate = 1.5;
-	/* weights[i][j] is the weight on edge from node i -> j. */
+	private final double learning_rate = 0.5;
+	/* weights[i][j] is the weight on edge from node i -> j. 
+	 * as a special case, weights[i][i] is the threshold value
+	 * of the perceptron i. */
 	private final double[][] weights;
-	/* out_edges[i] is a list of edges from i. */
-	private final LinkedList<Integer>[] out_edges;
-	/* in_edges[i] is a list of edges to i. */
-	private final LinkedList<Integer>[] in_edges;
+	/* outgoingEdges[i] is a list of edges from i. */
+	private final LinkedList<Integer>[] outgoingEdges;
+	/* incomingEdges[i] is a list of edges to i. */
+	private final LinkedList<Integer>[] incomingEdges;
 	/* layer[i] is a list of nodes in layer i. */
 	private final LinkedList<Integer>[] layer;
 	/* data set on which to make predictions. */
@@ -19,7 +20,17 @@ public class MultiLayerNeuralNet implements Classifier {
 	/* number of attributes in data set. */
 	private final int N;
 	/* number of nodes in the network. */
-	private final int num_nodes;
+	private final int numNodes;
+	
+	/** Calculates the error on the training examples of
+	 * data set d. */
+	private double error(DataSet d) {
+		if (d.numTrainExs == 0) return 0.0;
+		double sum = 0.0;
+		for (int i = 0; i < d.numTrainExs; i++)
+			sum += Math.abs(d.trainLabel[i] - predict(d.trainEx[i]));
+		return sum/d.numTrainExs;
+	}
 	
 	/** Runs an activation threshold function g on some
 	 * input value d.
@@ -31,50 +42,64 @@ public class MultiLayerNeuralNet implements Classifier {
 	/** Runs an activation threshold function g's derivative 
 	 * on some input value d.
 	 */
-	private double g_prime(double d) {
+	private double gPrime(double d) {
 		double g = g(d);
 		return g * (1.0 - g);
+	}
+	
+	/**
+	 * Trains the neural network on every example in data set d.
+	 */
+	private void backProp(DataSet d) {
+		for (int i = 0; i < d.numTrainExs; i++)
+			backProp(d.trainEx[i], d.trainLabel[i]);
 	}
 	
 	/** Trains the neural network on an example ex by using
 	 * the back propagation technique to adjust the net's weights.
 	 * Example ex is known to be of classification label.
 	 */
-	private void back_prop(int[] ex, int label) {		
-		double[] a = new double[this.num_nodes];
-		double[] in = new double[this.num_nodes];
-		double[] delta = new double[this.num_nodes];
+	private void backProp(int[] ex, int label) {
+		// output of each node
+		double[] a = new double[this.numNodes];
+		// input to each node
+		double[] in = new double[this.numNodes];
+		// delta for adjusting each edge weight
+		double[] delta = new double[this.numNodes];
 		
-		// First N nodes are input nodes
+		// first N nodes are input nodes
 		for (int i = 0; i < this.N; i++)
 			a[i] = ex[i];
 		
-		// Compute outputs by propagating inputs forward
+		// compute outputs by propagating inputs forward
 		for (int l = 1; l < this.layer.length; l++) {
 			for (int dest : this.layer[l]) {
-				for (int src : this.in_edges[dest]) {
+				for (int src : this.incomingEdges[dest]) {
 					in[dest] += this.weights[src][dest]*a[src];
 				}
+				// subtract threshold value
+				in[dest] -= this.weights[dest][dest];
 				a[dest] = g(in[dest]);
 			}
 		}
-		if ((int)Math.round(a[this.num_nodes - 1]) == label) return;
+		// return if accurate prediction
+		if (predict(a[this.numNodes - 1]) == label) return;
 		
-		// Compute deltas by propagating backward
-		// Begin with delta of output layer
-		delta[this.num_nodes - 1] = 
-				g_prime(in[this.num_nodes - 1])*(label - a[this.num_nodes - 1]); 
-		for (int l = this.layer.length - 1; l > 0; l--) {
+		// compute deltas by propagating backward
+		// degin with delta of output layer as base case
+		delta[this.numNodes - 1] = 
+				gPrime(a[this.numNodes - 1])*(label - (int)Math.round(a[this.numNodes - 1])); 
+		for (int l = this.layer.length - 2; l >= 0; l--) {
 			for (int src : this.layer[l]) {
 				double sum = 0;
-				for (int dest : this.out_edges[src]) {
+				for (int dest : this.outgoingEdges[src]) {
 					sum += this.weights[src][dest]*delta[dest];
 				}
-				delta[src] = g_prime(in[src])*sum;
+				delta[src] = gPrime(a[src])*sum;
 			}
 		}
 		
-		// Adjust weights
+		// adjust weights
 		for (int i = 0; i < this.weights.length; i++) {
 			for (int j = 0; j < this.weights.length; j++) {
 				if (this.weights[i][j] == 0) continue;
@@ -83,62 +108,85 @@ public class MultiLayerNeuralNet implements Classifier {
 		}
 	}
 	
+	/** Returns a random weight for an edge. */
+	private double randomWeight() {
+		return 0.5 - Math.random();
+	}
+	
+	
+	/** Resets the weights of a neural network to avoid getting caught
+	 * in a local minimum.
+	 */
+	private void randomizeWeights() {
+		for (int i = 0; i < this.weights.length; i++) {
+			for (int j = i+1; j < this.weights.length; j++) {
+				this.weights[i][j] = randomWeight();
+			}
+		}
+	}
+	
 	/** Constructor for the MultiLayerNeuralNet class that 
 	 * creates a multi-layer, feed-forward neural network 
 	 * from a data set.
 	 */
-	public MultiLayerNeuralNet(DataSet d) {
+	@SuppressWarnings("unchecked")
+	public MultiLayerNeuralNet(DataSet d) {		
 		this.d = d;
 		this.N = this.d.numAttrs;
-		int num_hidden = 4;
-		this.num_nodes = this.N + num_hidden + 1;
-		this.weights = new double[this.num_nodes][this.num_nodes];
-		this.in_edges = (LinkedList<Integer>[]) new LinkedList[this.num_nodes];
-		this.out_edges = (LinkedList<Integer>[]) new LinkedList[this.num_nodes];
-		this.layer = (LinkedList<Integer>[]) new LinkedList[3];
+		// number of nodes in hidden layer
+		int numHidden = this.N;
+		this.numNodes = this.N + numHidden + 1;
+		this.weights = new double[this.numNodes][this.numNodes];
+		randomizeWeights();
+		// create and initialize list of edges
+		this.incomingEdges = (LinkedList<Integer>[]) new LinkedList[this.numNodes];
+		this.outgoingEdges = (LinkedList<Integer>[]) new LinkedList[this.numNodes];
+		for (int i = 0; i < this.numNodes; i++) {
+			this.incomingEdges[i] = new LinkedList<Integer>();
+			this.outgoingEdges[i] = new LinkedList<Integer>();
+		}
+		// number of layers to be included
+		int numLayers = 3;
+		this.layer = (LinkedList<Integer>[]) new LinkedList[numLayers];
+		for (int i = 0; i < numLayers; i++)
+			this.layer[i] = new LinkedList<Integer>();
 
 		/* Create first layer and links to hidden layer. */
-		this.layer[0] = new LinkedList<Integer>();
 		for (int i = 0; i < this.N; i++) {
 			this.layer[0].add(i);
-			this.in_edges[i] = new LinkedList<Integer>();
-			this.out_edges[i] = new LinkedList<Integer>();
-			for (int j = this.N; j < this.N + num_hidden; j++) {
-				this.weights[i][j] = 5.0 - 10.0*Math.random();
-				this.out_edges[i].add(j);
-				
-				if (this.in_edges[j] == null) {
-					this.in_edges[j] = new LinkedList<Integer>(); 
-				}
-				this.in_edges[j].add(i);
+			// add incoming and outgoing edges
+			for (int j = this.N; j < this.N + numHidden; j++) {
+				this.outgoingEdges[i].add(j);
+				this.incomingEdges[j].add(i);
 			}
 		}
 		
 		/* Create second layer and links to third layer. */
-		this.in_edges[this.num_nodes - 1] = new LinkedList<Integer>();
-		this.out_edges[this.num_nodes - 1] = new LinkedList<Integer>();
-		this.layer[1] = new LinkedList<Integer>();
-		for (int j = this.N; j < this.N + num_hidden; j++) {
-			this.layer[1].add(j);
-			this.weights[j][this.num_nodes - 1] = 5.0 - 10.0*Math.random();
-			
-			this.in_edges[this.num_nodes - 1].add(j);
-			
-			if (this.out_edges[j] == null) {
-				this.out_edges[j] = new LinkedList<Integer>(); 
-			}
-			this.out_edges[j].add(this.num_nodes - 1);
+		for (int i = this.N; i < this.N + numHidden; i++) {
+			this.layer[1].add(i);
+			this.incomingEdges[this.numNodes - 1].add(i);
+			this.outgoingEdges[i].add(this.numNodes - 1);
 		}
 		
 		/* Create list of third layer (output node). */ 
-		this.layer[2] = new LinkedList<Integer>();
-		this.layer[2].add(this.num_nodes - 1);
-		
-		/* Train on the data set. */
-		for (int t = 0; t < 5000; t++) {
-			for (int i = 0; i < this.d.numTrainExs; i++) {
-				back_prop(this.d.trainEx[i], this.d.trainLabel[i]);
+		this.layer[2].add(this.numNodes - 1);
+
+		// train neural net on each training example
+		// run until epsilon threshold error is breached
+		double epsilon = 0.05;
+		double lastError = Double.MAX_VALUE;
+		int maxRuns = 100;
+		for (int runs = 0; runs < maxRuns; runs++) {
+			backProp(this.d);
+			double error = error(this.d);
+			if (error < epsilon) 
+				break;
+			if (error >= lastError) {
+				randomizeWeights();
+				lastError = Double.MAX_VALUE;
 			}
+			else
+				lastError = error;
 		}
 	}
 
@@ -148,8 +196,8 @@ public class MultiLayerNeuralNet implements Classifier {
      * prediction, i.e., 0 or 1.
      */
     public int predict(int[] ex) {
-    	double[] a = new double[this.num_nodes];
-		double[] in = new double[this.num_nodes];
+    	double[] a = new double[this.numNodes];
+		double[] in = new double[this.numNodes];
 		
 		// First N nodes are input nodes
 		for (int i = 0; i < this.N; i++)
@@ -158,7 +206,7 @@ public class MultiLayerNeuralNet implements Classifier {
     	// Compute outputs by propagating inputs forward
     	for (int l = 1; l < this.layer.length; l++) {
     		for (int dest : this.layer[l]) {
-    			for (int src : this.in_edges[dest]) {
+    			for (int src : this.incomingEdges[dest]) {
     				in[dest] += this.weights[src][dest]*a[src];
     			}
     			a[dest] = g(in[dest]);
@@ -166,7 +214,15 @@ public class MultiLayerNeuralNet implements Classifier {
     	}
     	    	
     	// Return based on output of sigmoid function
-    	return (int)Math.round(a[this.num_nodes - 1]);
+    	return predict(a[this.numNodes - 1]);
+    }
+    
+    /** Makes a prediction based on some input value a, which
+     * should--in practice--be the output value of the final
+     * perceptron.
+     */
+    private int predict(double a) {
+    	return (int)Math.round(a);
     }
 
     /** This method should return a very brief but understandable
