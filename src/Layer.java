4319da06a0d816a4e3c18e7c2455ad637c188ac9
Chaewon Jeong <lalae0228@gmail.com>
2021-05-23 19:26:10 +0900

    Add Variable, Method in Layer Class
    Add 'bestWeights'
    	in Layer
    
    Add assignBestWeights(), assignWeights(),
    	updateWeights(), getNumNodes()
    	in Layer

--- src/Layer.java
+++ src/Layer.java
@@ -7,6 +7,7 @@ public class Layer {
 	 * as a special case, weights[i][i] is the threshold value
 	 * of the perceptron i. */
 	private double[][] weights;
+	private double[][] bestWeights;
 	/* outgoingEdges[i] is a list of edges from i. */
 	private final LinkedList<Integer>[] outgoingEdges;
 	/* incomingEdges[i] is a list of edges to i. */
@@ -26,6 +27,7 @@ public class Layer {
 		initEdges();
 		
 		this.weights = new double[this.numNodes][this.nextNumNodes];
+		this.bestWeights = new double[this.numNodes][this.nextNumNodes];
 		randomizeWeights();
 	}
 
@@ -52,22 +54,6 @@ public class Layer {
 		}
 	}
 	
-	/** Returns a random weight for an edge. */
-	private double randomWeight() {
-		return 0.5 - Math.random();
-	}
-	
-	/** Resets the weights of a neural network to avoid getting caught
-	 * in a local minimum.
-	 */
-	private void randomizeWeights() {
-		for (int i = 0; i < this.numNodes; i++) {
-			for (int j = 0; j < this.nextNumNodes; j++) {
-				this.weights[i][j] = randomWeight();
-//				this.weights[j][i] = this.weights[i][j];
-			}
-		}
-	}
 
 	public void setWeight(int src, int dest, double weight) {
 		this.weights[src][dest] = weight;
@@ -85,4 +71,34 @@ public class Layer {
 		return this.outgoingEdges[src];
 	}
 	
+	public int getNumNodes() {
+		return this.numNodes;
+	}
+	
+	/** Returns a random weight for an edge. */
+	private double randomWeight() {
+		return 0.5 - Math.random();
+	}
+	
+	/** Resets the weights of a neural network to avoid getting caught
+	 * in a local minimum.
+	 */
+	public void randomizeWeights() {
+		for (int i = 0; i < this.numNodes; i++) {
+			for (int j = 0; j < this.nextNumNodes; j++) {
+				this.weights[i][j] = randomWeight();
+			}
+		}
+	}
+	
+	public void assignBestWeights() {
+		this.bestWeights = this.weights;
+	}
+	public void assignWeights() {
+		this.weights = this.bestWeights;
+	}
+	public void updateWeights() {
+		for (int i = 0; i < this.weights.length; i++)
+			System.arraycopy(this.weights[i], 0, this.bestWeights[i], 0, this.weights[i].length);
+	}
 }
