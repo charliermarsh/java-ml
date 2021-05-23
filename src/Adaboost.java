51b8960758b7595a9f33314df23f20de8999e6fc
SHIN-kh <38698921+SHIN-kh@users.noreply.github.com>
2021-05-23 18:00:39 +0900

    make pickRandomNumber method
    make pickRandomNumber method in DecisionForest class 
    extract method
    We reduce duplicated code using

--- src/Adaboost.java
+++ src/Adaboost.java
@@ -15,6 +15,12 @@ public class Adaboost implements Classifier{
     Random random;
     DecisionTree[] forest;
 
+    private ArrayList<Integer> pickRandomNumber(int number) {
+        ArrayList<Integer> result = new ArrayList<Integer>(number);
+        for (int i = 0; i < number; i++) { result.add(i); }
+        return result;
+    }
+    
     public Adaboost(DataSet data, int forestSize) {
         random = new Random();
 
@@ -27,10 +33,8 @@ public class Adaboost implements Classifier{
          *
          * Picks a random number of attributes/examples to train each tree.
          */
-        ArrayList<Integer> attributes = new ArrayList<Integer>(data.numAttrs);
-        ArrayList<Integer> examples = new ArrayList<Integer>(data.numTrainExs);
-        for (int i = 0; i < data.numAttrs; i++) { attributes.add(i); }
-        for (int i = 0; i < data.numTrainExs; i++) { examples.add(i); }
+        ArrayList<Integer> attributes = pickRandomNumber(data.numAttrs);
+        ArrayList<Integer> examples = pickRandomNumber(data.numTrainExs);
 
         //Train each tree by choosing a subset of features. Actually just using
         //every feature in this case.
