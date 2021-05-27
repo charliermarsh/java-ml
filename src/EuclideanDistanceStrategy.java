81caecd000b7bc8d3cb9a5540c1008928cc1b615
ParkSeongReol <dortm951753@gmail.com>
2021-05-27 16:39:26 +0900

    linearForwardDistanceUpdate/ distanceCalculation
    replace temp with query
    
    need another distance calculation strategy for
    linearForwardDistanceUpdate

--- src/EuclideanDistanceStrategy.java
+++ src/EuclideanDistanceStrategy.java
@@ -4,4 +4,8 @@ public class EuclideanDistanceStrategy implements DistanceStrategy{
 		double distance = Math.abs(pos1 - pos2);
 		return distance;
 	}
+	public double calcPlusDistance(int pos1, int pos2) {
+		double distance = Math.abs(pos1 + pos2);
+		return distance;
+	}
 }
