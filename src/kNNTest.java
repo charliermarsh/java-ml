f589be359f13e4ce1ccd36e6276707c29e3d3cbb
ji-one <ds020228@naver.com>
2021-06-06 18:50:43 +0900

    Purpose: Calculates the distance between two vectors Input: getDistance get distance between {1,0,0,1} and {1,0,1,0} Expected: 	return 2

--- src/kNNTest.java
+++ src/kNNTest.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 public class kNNTest {
 	/*
 	 * Purpose: Test knn class member variable initialization in knn constructor
@@ -20,6 +22,30 @@ public class kNNTest {
 		assertEquals(dataset, knn.getDataSet());
 		assertEquals(strategy, knn.getStrategy());
 	}
+	
+	/*
+	 * Purpose: Calculates the distance between two vectors
+	 * Input: getDistance get distance between {1,0,0,1} and {1,0,1,0}
+	 * Expected:
+	 * 		return 2
+	 */
+	@Test
+	public void testgetDistance() throws Exception {
+		DataSetInput input = new FileInput("./data/knn_test_dna");
+		DataSet dataset = new BinaryDataSet(input);
+		Strategy strategy = new Strategy(new EuclideanDistance(), new kFoldCrossValidation());
+		kNN knn = new kNN(dataset, strategy);
+		
+		Method method = knn.getClass().getDeclaredMethod("getDistance", int[].class, int[].class); 
+		method.setAccessible(true);
+		
+		int[] vector1 = {1,0,0,1};
+		int[] vector2 = {1,0,1,0};
+		
+		double distance = (double)method.invoke(knn, vector1, vector2);
+		assertEquals(distance, 2, 0.00001);
+	}
+	
 	
 	
 }
