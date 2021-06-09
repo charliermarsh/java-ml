6ad814fa9090d086afcd46ea982b5f70844b7657
SHIN-kh <38698921+SHIN-kh@users.noreply.github.com>
2021-06-09 22:53:09 +0900

    Update TreeTest.java
    Clean code

--- src/TreeTest.java
+++ src/TreeTest.java
@@ -122,66 +122,7 @@ public class TreeTest {
         assertEquals((100*correct / crossEx.length),100);
 
 	}
-	/**
-	* Purpose:test whether the learning algorithm is well applied using a tree with a prediction ratio of 100% in other constructor .
-	* Input: DecisionTree(constructor) Create DecisionTree
-	* Expected:
-	* 			100 * correct / crossEx.length (prediction ratio) = 100
-	*/
-	@Test
-	public void testDecisionTreeConstructor2() throws Exception {
-        String filestem = "data/testTreedata";
-        
-        DiscreteDataSet d = new DiscreteDataSet(filestem);
 
-        Random random = new Random();
-        for (int i = 0; i < d.numTrainExs; i++) {
-            int swap = random.nextInt(d.numTrainExs - i);
-            int[] tempEx = d.trainEx[swap];
-            d.trainEx[swap] = d.trainEx[d.numTrainExs - i - 1];
-            d.trainEx[d.numTrainExs - i - 1] = tempEx;
-            int tempLabel = d.trainLabel[swap];
-            d.trainLabel[swap] = d.trainLabel[d.numTrainExs - i - 1];
-            d.trainLabel[d.numTrainExs - i - 1] = tempLabel;
-        }
-
-        int crossSize = d.numTrainExs/8;
-
-        int[][] crossEx = new int[crossSize][];
-        int[] crossLabel = new int[crossSize];
-
-        int[][] dEx = new int[d.numTrainExs - crossSize][];
-        int[] dLabel = new int[d.numTrainExs - crossSize];
-
-        for (int i = 0; i < d.numTrainExs - crossSize; i++) {
-            dEx[i] = d.trainEx[i];
-            dLabel[i] = d.trainLabel[i];
-        }
-
-        for (int i = 0; i < crossSize; i++) {
-            crossEx[i] = d.trainEx[d.numTrainExs - i - 1];
-            crossLabel[i] = d.trainLabel[d.numTrainExs - i - 1];
-        }
-
-        d.numTrainExs = dEx.length;
-        d.trainEx = dEx;
-        d.trainLabel = dLabel;
-        
-        HashSet<Integer> attributes = new HashSet<Integer>(d.numAttrs);
-    	for (int i = 0; i < d.numAttrs; i++) { attributes.add(i); }
-        ArrayList<Integer> examples = new ArrayList<Integer>(d.numTrainExs);
-        for (int i = 0; i < d.numTrainExs; i++) { examples.add(i); }
-        Classifier c = new DecisionTree(d, attributes, examples, false);
-
-        int correct = 0;
-        for (int ex = 0; ex < crossEx.length; ex++) {
-            if (c.predict(crossEx[ex]) == crossLabel[ex])
-                correct++;
-        }
-	
-        assertEquals((100*correct / crossEx.length),100);
-
-	}
 	/**
 	* Purpose:test whether the learning algorithm is well applied using a tree with a prediction ratio of 100% in other constructor .
 	* Input: DecisionTree(constructor) Create DecisionTree
