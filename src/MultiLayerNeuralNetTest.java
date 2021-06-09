fa4d2a8cfbb96fceab86d7f11839eae9a4335323
Chaewon Jeong <codnjs8221@naver.com>
2021-06-09 21:45:53 +0900

    Create JUnit Test files
    Create JUnit Test files -
    		SingleLayerNeuralNetTest and MultiLayerNeuralNetTest

---
+++ src/MultiLayerNeuralNetTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MultiLayerNeuralNetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/SingleLayerNeuralNetTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SingleLayerNeuralNetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
